6c42c93f62ea68340583af6456c5636c2c94d37c
camposzika <lucas.paredes.campos@escola.pr.gov.br>
2026-06-26 11:39:40 -0300

    Update script.js

--- campos ds/script.js
+++ campos ds/script.js
@@ -1,3 +1,11 @@
 function revelarMensagem() {
-    document.getElementById("mensagem").innerText = "Olá Neymar!";
+    document.getElementById("mensagem").innerText = 
+        "Olá, você alterou a mensagem chamando uma função JS";
+}
+
+function mostrarNome() {
+    let nome = document.getElementById("nomeUsuario").value;
+
+    document.getElementById("resultadoNome").innerText = 
+        "Bem-vindo, " + nome + "!";
 }
